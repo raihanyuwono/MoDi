387fe3b46726fba521614fb7aa60fa496efdb4ed
raihanyuwono <raihanyuwono@hotmail.com>
2023-06-17 09:23:13 +0700

    add container newStory

--- src/components/TabContents.jsx
+++ src/components/TabContents.jsx
@@ -1,4 +1,5 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import ContainerMain from './details/ContainerMain';
 
 const articleDummy = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur similique, beatae ab nihil nobis sed eaque deleniti esse unde eveniet veniam omnis quam ipsam pariatur debitis voluptas nisi voluptate facere!
 Lorem ipsum dolor sit amet consectetur adipisicing elit. \nConsequuntur similique, beatae ab nihil nobis sed eaque deleniti esse unde eveniet veniam omnis quam ipsam pariatur debitis voluptas nisi voluptate facere!
@@ -13,21 +14,21 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. \nConsequuntur similiqu
 Lorem ipsum dolor sit amet consectetur adipisicing elit. \nConsequuntur similique, beatae ab nihil nobis sed eaque deleniti esse unde eveniet veniam omnis quam ipsam pariatur debitis voluptas nisi voluptate facere!
 `;
 
-function TabArticles() {
+function TabContents() {
   return (
     <Tabs isFitted variant={'enclosed-colored'}>
-      <TabPanels>
-        <TabPanel>1.{articleDummy}</TabPanel>
-        <TabPanel>2.{articleDummy}</TabPanel>
-        <TabPanel>3.{articleDummy}</TabPanel>
-      </TabPanels>
-      <TabList pos={"fixed"} bottom={0} w={"full"}>
+      <TabList pos={"sticky"} top={"4rem"} w={"full"}>
         <Tab>One</Tab>
         <Tab>Two</Tab>
         <Tab>Three</Tab>
       </TabList>
+      <TabPanels>
+        <TabPanel py={"0px"}><ContainerMain/></TabPanel>
+        <TabPanel py={"0px"}>2.{articleDummy}</TabPanel>
+        <TabPanel py={"0px"}>3.{articleDummy}</TabPanel>
+      </TabPanels>
     </Tabs>
   );
 }
 
-export default TabArticles;
+export default TabContents;
---
+++ src/pages/NewStory.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+import ContainerMain from "../components/details/ContainerMain";
+
+function NewStory(){
+    return (
+        <>
+            <NavBar/>
+            <ContainerMain />
+        </>
+    );
+}
+
+export default NewStory;
